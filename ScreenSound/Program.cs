ff4583aeeee5c186be11d9375e1a4bc2094ebfe5
RA4Z <rraz639@gmail.com>
2023-06-20 16:14:51 -0300

    Criando os objetos com as músicas

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,4 +1,11 @@
-﻿// nome
-// artista
-// duracao
-// disponivel
+﻿Musica musica1 = new Musica();
+Musica musica2 = new Musica();
+
+musica1.nome = "Detroit Rock City";
+musica1.artista = "Kiss";
+
+Console.WriteLine($"Música: {musica1.nome}");
+Console.WriteLine($"Artista: {musica1.artista}");
+
+musica2.nome = "Chop Suey!";
+musica2.artista = "System of A Down";
