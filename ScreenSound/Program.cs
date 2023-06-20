e1eb7a2c77c3d3ca224b019e044bc93d10b8e97d
RA4Z <rraz639@gmail.com>
2023-06-20 16:19:59 -0300

    Adicionando função na classe da música

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -3,9 +3,13 @@ Musica musica2 = new Musica();
 
 musica1.nome = "Detroit Rock City";
 musica1.artista = "Kiss";
-
-Console.WriteLine($"Música: {musica1.nome}");
-Console.WriteLine($"Artista: {musica1.artista}");
+musica1.duracao = 255;
+musica1.disponivel = false;
 
 musica2.nome = "Chop Suey!";
 musica2.artista = "System of A Down";
+musica2.duracao = 176;
+musica2.disponivel = true;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
