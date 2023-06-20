1356054d1e68b3caca45cc87f61302a7035cb8d1
RA4Z <rraz639@gmail.com>
2023-06-20 16:42:08 -0300

    Refatorando as propriedades para o padrão C#

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,13 +1,13 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Detroit Rock City";
-musica1.artista = "Kiss";
-musica1.duracao = 255;
+musica1.Nome = "Detroit Rock City";
+musica1.Artista = "Kiss";
+musica1.Duracao = 255;
 musica1.Disponivel = true;
 
 Musica musica2 = new Musica();
-musica2.nome = "Chop Suey!";
-musica2.artista = "System of A Down";
-musica2.duracao = 176;
+musica2.Nome = "Chop Suey!";
+musica2.Artista = "System of A Down";
+musica2.Duracao = 176;
 musica2.Disponivel= false;
 
 musica1.ExibirFichaTecnica();
